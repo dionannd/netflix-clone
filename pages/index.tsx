b24473034e6bbca620704d5c03ca868300d6b124
dionannd <dionananda77@gmail.com>
2022-04-22 22:15:45 +0700

    fix(eslint): error when deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { movieState } from '../atoms/modalAtom'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
 import Row from '../components/Row'
